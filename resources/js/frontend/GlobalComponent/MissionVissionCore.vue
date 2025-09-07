0f80151fd5efac9d3a23041720ba9e6b2fb89b75
tasnim-jawad <tasnimulh453@gmail.com>
2025-09-07 19:09:01 +0600

    home page

--- resources/js/frontend/GlobalComponent/MissionVissionCore.vue
+++ resources/js/frontend/GlobalComponent/MissionVissionCore.vue
@@ -1,31 +1,51 @@
 <template>
-  <section class="th-blog-wrapper space-top overflow-hidden space-extra-bottom">
+  <section class="th-blog-wrapper space-top overflow-hidden space-extra-bottom mission">
     <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-7">
+          <div class="title-area text-center">
+            <p
+              class="sub-title fadeinup wow"
+              data-wow-duration="1.5s"
+              data-wow-delay="0.1s"
+            >
+              <span class="double-line"></span> Mission Vision & Core Values
+            </p>
+            <h2
+              class="sec-title fadeinup wow"
+              data-wow-duration="1.5s"
+              data-wow-delay="0.3s"
+            >
+              Our Mission
+            </h2>
+          </div>
+        </div>
+      </div>
       <div class="row gx-60">
         <div class="col-xxl-12 col-lg-12">
           <div class="row gy-30 gx-30 justify-content-center mb-60">
-            <div class="col-xxl-5 col-xl-12">
+            <div class="col-xxl-6 col-xl-12">
               <div
                 class="team-card style-2 me-xxl-4 background-image"
                 data-bg-src="/assets/frontend/img/shape/team-2-bg-shape.png"
               >
                 <div class="team-img">
                   <img
-                    src="/assets/frontend/img/team/team_2_3.png"
+                    src="/assets/frontend/img/popular/popular-1-6.jpg"
                     alt="Team"
                   />
                 </div>
                 
               </div>
             </div>
-            <div class="col-xxl-7 col-xl-12">
+            <div class="col-xxl-6 col-xl-12">
               <div class="team-wrap">
-                <div class="top-wrap">
+                <!-- <div class="top-wrap">
                   <div class="title-wrap">
                     <h3 class="team-about_title">Our Mission</h3>
                   </div>
-                </div>
-                <p class="team-about_text">
+                </div> -->
+                <p class="team-about_text mt-5">
                   Zulia Era is one of the best online resources for learning
                   about technology in the medical sphere. There’s a real sense
                   skepticiesm cillum dolore eu fugiat nulla pariatur.
@@ -41,47 +61,6 @@
                 <a href="agency.html" class="th-btn radius bg-theme"
                   >Contact With Agent</a
                 >
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <h4 class="box-title">
-                Problem solved for real estate community
-              </h4>
-              <div class="check-list mb-50">
-                <ul>
-                  <li>Diversity investigation for royal been.</li>
-                  <li>Finding the Right Home or Buyer</li>
-                  <li>Navigating Legal & Paperwork Issues</li>
-                  <li>Negotiation Challenges.</li>
-                  <li>Financing and Mortgage Assistance</li>
-                </ul>
-              </div>
-              <h4 class="box-title">Impact</h4>
-              <p class="team-about_text mb-40">
-                Real estate agents play a critical role in solving various
-                problems within the real estate community. Their expertise not
-                only helps individual buyers and sellers but also positively
-                impacts the overall housing market, local economy, and community
-                development. Below are key problems agents solve and how their
-                work improves the real estate community.
-              </p>
-              <div class="check-list">
-                <ul>
-                  <li>
-                    Prevents overpricing and underpricing, maintaining property
-                    values.
-                  </li>
-                  <li>
-                    Ensures a balanced housing market, avoiding price crashes or
-                    overinflation.
-                  </li>
-                  <li>Navigating Legal & Paperwork Issues</li>
-                  <li>
-                    Helps maintain neighborhood stability and property values..
-                  </li>
-                </ul>
               </div>
             </div>
           </div>
@@ -89,33 +68,32 @@
       </div>
     </div>
   </section>
-
-  <section class="th-blog-wrapper space-top overflow-hidden space-extra-bottom">
+  <section class="th-blog-wrapper space-top overflow-hidden space-extra-bottom mission">
     <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-7">
+          <div class="title-area text-center">
+            <h2
+              class="sec-title fadeinup wow"
+              data-wow-duration="1.5s"
+              data-wow-delay="0.3s"
+            >
+              Our Vission
+            </h2>
+          </div>
+        </div>
+      </div>
       <div class="row gx-60">
         <div class="col-xxl-12 col-lg-12">
           <div class="row gy-30 gx-30 justify-content-center mb-60">
-            <div class="col-xxl-5 col-xl-12">
-              <div
-                class="team-card style-2 me-xxl-4 background-image"
-                data-bg-src="/assets/frontend/img/shape/team-2-bg-shape.png"
-              >
-                <div class="team-img">
-                  <img
-                    src="/assets/frontend/img/team/team_2_3.png"
-                    alt="Team"
-                  />
-                </div>
-              </div>
-            </div>
-            <div class="col-xxl-7 col-xl-12">
+            <div class="col-xxl-6 col-xl-12">
               <div class="team-wrap">
-                <div class="top-wrap">
+                <!-- <div class="top-wrap">
                   <div class="title-wrap">
-                    <h3 class="team-about_title">Our Vision</h3>
+                    <h3 class="team-about_title">Our Mission</h3>
                   </div>
-                </div>
-                <p class="team-about_text">
+                </div> -->
+                <p class="team-about_text mt-5">
                   Zulia Era is one of the best online resources for learning
                   about technology in the medical sphere. There’s a real sense
                   skepticiesm cillum dolore eu fugiat nulla pariatur.
@@ -133,45 +111,18 @@
                 >
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <h4 class="box-title">
-                Problem solved for real estate community
-              </h4>
-              <div class="check-list mb-50">
-                <ul>
-                  <li>Diversity investigation for royal been.</li>
-                  <li>Finding the Right Home or Buyer</li>
-                  <li>Navigating Legal & Paperwork Issues</li>
-                  <li>Negotiation Challenges.</li>
-                  <li>Financing and Mortgage Assistance</li>
-                </ul>
-              </div>
-              <h4 class="box-title">Impact</h4>
-              <p class="team-about_text mb-40">
-                Real estate agents play a critical role in solving various
-                problems within the real estate community. Their expertise not
-                only helps individual buyers and sellers but also positively
-                impacts the overall housing market, local economy, and community
-                development. Below are key problems agents solve and how their
-                work improves the real estate community.
-              </p>
-              <div class="check-list">
-                <ul>
-                  <li>
-                    Prevents overpricing and underpricing, maintaining property
-                    values.
-                  </li>
-                  <li>
-                    Ensures a balanced housing market, avoiding price crashes or
-                    overinflation.
-                  </li>
-                  <li>Navigating Legal & Paperwork Issues</li>
-                  <li>
-                    Helps maintain neighborhood stability and property values..
-                  </li>
-                </ul>
+            <div class="col-xxl-6 col-xl-12">
+              <div
+                class="team-card style-2 me-xxl-4 background-image"
+                data-bg-src="/assets/frontend/img/shape/team-2-bg-shape.png"
+              >
+                <div class="team-img">
+                  <img
+                    src="/assets/frontend/img/popular/popular-1-6.jpg"
+                    alt="Team"
+                  />
+                </div>
+                
               </div>
             </div>
           </div>
@@ -179,33 +130,46 @@
       </div>
     </div>
   </section>
-
-  <section class="th-blog-wrapper space-top overflow-hidden space-extra-bottom">
+  <section class="th-blog-wrapper space-top overflow-hidden space-extra-bottom mission">
     <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-xl-7">
+          <div class="title-area text-center">
+            <h2
+              class="sec-title fadeinup wow"
+              data-wow-duration="1.5s"
+              data-wow-delay="0.3s"
+            >
+              Core Values
+            </h2>
+          </div>
+        </div>
+      </div>
       <div class="row gx-60">
         <div class="col-xxl-12 col-lg-12">
           <div class="row gy-30 gx-30 justify-content-center mb-60">
-            <div class="col-xxl-5 col-xl-12">
+            <div class="col-xxl-6 col-xl-12">
               <div
                 class="team-card style-2 me-xxl-4 background-image"
                 data-bg-src="/assets/frontend/img/shape/team-2-bg-shape.png"
               >
                 <div class="team-img">
                   <img
-                    src="/assets/frontend/img/team/team_2_3.png"
+                    src="/assets/frontend/img/popular/popular-1-6.jpg"
                     alt="Team"
                   />
                 </div>
+                
               </div>
             </div>
-            <div class="col-xxl-7 col-xl-12">
+            <div class="col-xxl-6 col-xl-12">
               <div class="team-wrap">
-                <div class="top-wrap">
+                <!-- <div class="top-wrap">
                   <div class="title-wrap">
-                    <h3 class="team-about_title">Our Core Values</h3>
+                    <h3 class="team-about_title">Our Mission</h3>
                   </div>
-                </div>
-                <p class="team-about_text">
+                </div> -->
+                <p class="team-about_text mt-5">
                   Zulia Era is one of the best online resources for learning
                   about technology in the medical sphere. There’s a real sense
                   skepticiesm cillum dolore eu fugiat nulla pariatur.
@@ -221,47 +185,6 @@
                 <a href="agency.html" class="th-btn radius bg-theme"
                   >Contact With Agent</a
                 >
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <h4 class="box-title">
-                Problem solved for real estate community
-              </h4>
-              <div class="check-list mb-50">
-                <ul>
-                  <li>Diversity investigation for royal been.</li>
-                  <li>Finding the Right Home or Buyer</li>
-                  <li>Navigating Legal & Paperwork Issues</li>
-                  <li>Negotiation Challenges.</li>
-                  <li>Financing and Mortgage Assistance</li>
-                </ul>
-              </div>
-              <h4 class="box-title">Impact</h4>
-              <p class="team-about_text mb-40">
-                Real estate agents play a critical role in solving various
-                problems within the real estate community. Their expertise not
-                only helps individual buyers and sellers but also positively
-                impacts the overall housing market, local economy, and community
-                development. Below are key problems agents solve and how their
-                work improves the real estate community.
-              </p>
-              <div class="check-list">
-                <ul>
-                  <li>
-                    Prevents overpricing and underpricing, maintaining property
-                    values.
-                  </li>
-                  <li>
-                    Ensures a balanced housing market, avoiding price crashes or
-                    overinflation.
-                  </li>
-                  <li>Navigating Legal & Paperwork Issues</li>
-                  <li>
-                    Helps maintain neighborhood stability and property values..
-                  </li>
-                </ul>
               </div>
             </div>
           </div>
@@ -275,4 +198,17 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+.mission .team-card.style-2 {
+    padding: 0 0 0 0;
+}
+.mission .team-card.style-2 .team-img{
+    padding: 0 0 0 0;
+}
+.mission .team-card.style-2 img{
+    padding: 0 0 0 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
